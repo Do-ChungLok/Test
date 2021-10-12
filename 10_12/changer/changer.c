dda43b5784ce627b99d8ced00bb4ad259e5b2c8f
Wheel <whlee@auto-it.co.kr>
2021-10-12 17:41:21 +0900

    fix: complete code changer.c for reference..

--- 10_12/changer/changer.c
+++ 10_12/changer/changer.c
@@ -6,18 +6,50 @@
 #include <stdio.h>
 #include <string.h>
 
+int counting_lines(char* ptr)
+{
+	int lines = 0;
+	char* p = ptr;
+
+	while (*p != 0) {
+		if (*p == '\n') { lines++; ptr=p; }
+		//putchar(*ptr);
+		p++;
+	}
+	if (ptr != p-1) lines++;
+
+	printf("total lines =%d\n", lines);
+	return lines;
+}
+
+char* find_line_start(char* ptr, int want_line)
+{
+	int lines = 0;
+	while (ptr != 0 && want_line > (lines+1)) {
+		if (*ptr == '\n') lines++;
+		ptr++;
+	}
+	return ptr;
+}
+
+void print_content(char* ptr)
+{
+	printf("===========\n");
+	while (*ptr != 0) {
+		putchar(*ptr++);
+	}
+	printf("===========\n");
+}
 
 int main(int argc, char *argv[]) {
     int fd;
     caddr_t addr;
     struct stat statbuf;
-    int num1;
+    int num1 = 0;
     char str[100];
     int count;
     int i;
     off_t len;
-    
-
 
     if(argc != 2) {
         fprintf(stderr, "Usage : %s filename\n", argv[0]);
@@ -35,74 +67,112 @@ int main(int argc, char *argv[]) {
     }
 
     // 파일을 메모리에 매핑한다. 
-    addr = mmap(NULL, statbuf.st_size, PROT_READ|PROT_WRITE, 
-                MAP_SHARED, fd, 0);
+	len = statbuf.st_size + 100;
+    addr = mmap(NULL, len, PROT_READ|PROT_WRITE, MAP_SHARED, fd, 0);
     if(addr == MAP_FAILED) {
         perror("mmap");
         exit(1);
     }
     
-    for (len = 0; len < statbuf.st_size; ++len)
-        putchar(addr[len]);
-     
-    close(fd);
-    //munmap(p, statbuf.st_size);
-    
+	int max_lines = counting_lines((char*)addr);
 
-    printf("'2'를 입력하세요 : ");
-    scanf("%d", &num1);
-    
+	do {
+		printf("변경할 라인은? ");
+		scanf("%d", &num1);
+	} while( num1 <= 0 || num1 > max_lines);
+
+	printf("변환할 문자열을 입력하세요 : ");
+	scanf("%s", str);
+
+	// add newline character for input strings.
+	strcat(str, "\n");
+
+	//
+	char* p_next = find_line_start(addr, num1+1);
+	char* p = find_line_start(addr, num1);
+
+	int tobe_str_len = strlen(str);
+	int asis_str_len = (int)(p_next - p);
+
+	printf("> 이전 문자열 길이 = %d\n", asis_str_len);
+	printf("> 입력 문자열 길이 = %d\n", tobe_str_len);
+
+	print_content(addr);
+
+	char *d, *s;
+	if (tobe_str_len < asis_str_len) {
+		memcpy(p, str, tobe_str_len);
+		for (d=p+tobe_str_len, s=p_next; *s; d++, s++) {
+			*d = *s;
+		}
+		for (; *d; d++)	*d = '\0';
+	} else {
+		s = addr + statbuf.st_size;
+		for (d = s + (tobe_str_len-asis_str_len); s>p_next; s--, d--) {
+			*d = *s;
+		}
+		memcpy(p, str, tobe_str_len);
+	}
+
+	print_content(addr);
+
+    munmap(addr, len);
+    close(fd);
+
+#if 0 
     switch(num1){
     
     case 2:
     {
-    
-   	printf("변환할 문자열을 입력하세요 : ");
-    	scanf("%s",str);
 
-     	//1행
-	for(int i=0; i < statbuf.st_size; i++){
-    		if(addr[i] == '\n') 
-    			count = i;
-    			printf("%d",count);
+		printf("변환할 문자열을 입력하세요 : ");
+		scanf("%s",str);
+
+		//1행
+		for(int i=0; i < statbuf.st_size; i++){
+			if(addr[i] == '\n') 
+				count = i;
+			printf("%d",count);
 
 
-		1행 원래 문자열 그대로
-		for(count=0; count-1; count++){
-			printf("%c",addr[count]);
+			//1행 원래 문자열 그대로
+			for(count=0; count-1; count++){
+				printf("%c",addr[count]);
+			}
 		}
-	}
-	
-	
-
-	//2행
-	for(int i=count+1; addr[i]; i++){ 
-		if(addr[i] == '\n')
-			count = i;
-	
-		2행에 입력한 문자열 치환
-		for(count; count-1; count++){
-			printf("%s",str);
-		}
-	}	
 
 
-	3행
-	for(int i=count+1; addr[i]; i++){
-		if(addr[i] == '\n')
-			count = i;
-	
-		//3행 원래 문자열 그대로
-		for(count=0; count-1; count++){
-			printf("%c",addr[count]);
-		}
-     	} 	
-     	
-    	break;
+
+		//2행
+		for(int i=count+1; addr[i]; i++){ 
+			if(addr[i] == '\n')
+				count = i;
+
+			//2행에 입력한 문자열 치환
+			for(count; count-1; count++){
+				printf("%s",str);
+			}
+		}	
+
+
+		//3행
+		for(int i=count+1; addr[i]; i++){
+			if(addr[i] == '\n')
+				count = i;
+
+			//3행 원래 문자열 그대로
+			for(count=0; count-1; count++){
+				printf("%c",addr[count]);
+			}
+		} 	
+
+		break;
     }	
     default:
     	printf("ERROR. 1~3 input plz\n");
     	break;
     }
+#endif
+
     return 0;
 }
