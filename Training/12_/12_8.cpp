8adda2ea758361ba0fab768eeded14082255b0b2
Wheel <whlee@auto-it.co.kr>
2021-11-30 18:36:32 +0900

    12-8 예제 참고용으로 수정후 커밋

--- Training/12_/12_8.cpp
+++ Training/12_/12_8.cpp
@@ -4,45 +4,44 @@
 
 #define STR_SIZE_MAX 51
 
-typedef struct {     // 길이, 단어배열 구조체 선언
+struct Word {     // 길이, 단어배열 구조체 선언
     int str_len; 
     char str[STR_SIZE_MAX]; 
-}Word; 
+};
 
-int compare(const void* first_word, const void* second_word){
+int compare(const void* first_word, const void* second_word)
+{
     Word* word1 = (Word*)first_word;
     Word* word2 = (Word*)second_word;
     
-    if(word1->str_len > word2->str_len){
+    if (word1->str_len > word2->str_len) {
         return 1;
-    }
-    
-    else if(word1->str_len < word2->str_len){
+    } else if (word1->str_len < word2->str_len) {
         return -1;
-    }
-    
-    else if(word1->str_len == word2->str_len){    // 두 단어 길이가 같을 경우  
+    } else if (word1->str_len == word2->str_len) {    // 두 단어 길이가 같을 경우  
         if(strcmp(word1->str, word2->str) < 0)    // 사전순으로 정렬
             return -1;
         else
             return 1;
-    }       
+    }
     return 0;
 }
 
-int main(void) { 
+int main(void)
+{ 
     int i, j, N;
     Word* list;
     
     scanf("%d", &N);
     
-    list = (Word*)calloc(N + 1, sizeof(Word));
+    //list = (Word*)calloc(N + 1, sizeof(Word));
+	list = new Word[N+1];
     
     for (i = 0; i < N; i++){ 
         scanf("%s", list[i].str);     // 단어 입력
         list[i].str_len = strlen(list[i].str);
         
-        for(j = 0; j < i; j++){        // 중복 제거
+        for(j = 0; j < i; j++){        // 중복 제거 ????????????????????
             if(strcmp(list[i].str, list[j].str) == 0){
                 i -= 1;
                 N -= 1;
@@ -53,9 +52,13 @@ int main(void) {
     
     qsort(list, N, sizeof(list[0]), compare);     // 정렬
     
+	printf("----- result \n");
     for (i = 0; i < N; i++){
         printf("%s\n", list[i].str); 
     }
+
+	delete[] list;
+
     return 0; 
 }
 
